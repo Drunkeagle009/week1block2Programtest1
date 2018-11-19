0d72c4b098c3a453d5b38da097e2fa6742d27df5
Drunkeagle009 <drunkeagle009@gmail.com>
2018-11-19 18:02:40 +0100

    added nav script to the menu button navigate to lvel 1

---
+++ Assets/scripts/navigate2Scene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class navigate2Scene : MonoBehaviour {
+    // making a place to put scene
+    [SerializeField]
+
+    string SceneToLoad;
+	
+    // the proces of loading the scene you inserted in the serializefield
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
